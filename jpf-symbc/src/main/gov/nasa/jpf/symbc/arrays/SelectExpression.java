f0f537ce99b4097824fd491ed446093209e8515c
R1kM <aymeric.fr@hotmail.fr>
2015-11-29 21:06:03 +0100

    Adding constructors for Select and StoreExpression

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/arrays/SelectExpression.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/arrays/SelectExpression.java
@@ -3,12 +3,23 @@ package gov.nasa.jpf.symbc.arrays;
 import gov.nasa.jpf.symbc.numeric.ConstraintExpressionVisitor;
 import gov.nasa.jpf.symbc.numeric.IntegerExpression;
 import gov.nasa.jpf.symbc.numeric.Expression;
+import gov.nasa.jpf.symbc.numeric.IntegerConstant;
 
 import java.util.Map;
 
 public class SelectExpression extends Expression {
     ArrayExpression ae;
     IntegerExpression index;
+
+    public SelectExpression(ArrayExpression ae, IntegerExpression ie) {
+        this.ae = ae;
+        this.index = ie;
+    }
+
+    public SelectExpression(ArrayExpression ae, int index) {
+        this.ae = ae;
+        this.index = new IntegerConstant(index);
+    }
 
     public int compareTo(Expression expr) {
         // unimplemented
--- jpf-symbc/src/main/gov/nasa/jpf/symbc/arrays/StoreExpression.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/arrays/StoreExpression.java
@@ -3,6 +3,7 @@ package gov.nasa.jpf.symbc.arrays;
 import gov.nasa.jpf.symbc.numeric.ConstraintExpressionVisitor;
 import gov.nasa.jpf.symbc.numeric.IntegerExpression;
 import gov.nasa.jpf.symbc.numeric.Expression;
+import gov.nasa.jpf.symbc.numeric.IntegerConstant;
 
 import java.util.Map;
 
@@ -10,6 +11,24 @@ public class StoreExpression extends Expression {
     ArrayExpression ae;
     IntegerExpression index;
     IntegerExpression value;
+
+    public StoreExpression(ArrayExpression ae, IntegerExpression ie, IntegerExpression value) {
+        this.ae = ae;
+        this.index = ie;
+        this.value = value;
+    }
+
+    public StoreExpression(ArrayExpression ae, int index, IntegerExpression value) {
+        this(ae, new IntegerConstant(index), value);
+    }
+
+    public StoreExpression(ArrayExpression ae, IntegerExpression ie, int value) {
+        this(ae, ie, new IntegerConstant(value));
+    }
+
+    public StoreExpression(ArrayExpression ae, int index, int value) {
+        this(ae, new IntegerConstant(index), new IntegerConstant(value));
+    }
 
     public int compareTo(Expression expr) {
         // unimplemented
