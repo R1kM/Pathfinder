e176cbee4cdf780a1c035ff68436bfe11fcfd6ed
R1kM <aymeric.fr@hotmail.fr>
2015-12-21 20:00:20 +0100

    Adding implementation of symbolic ARRAYLENGTH

---
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/bytecode/ARRAYLENGTH.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * Symbolic Pathfinder (jpf-symbc) is licensed under the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package gov.nasa.jpf.symbc.bytecode;
+
+import gov.nasa.jpf.symbc.arrays.ArrayExpression;
+import gov.nasa.jpf.symbc.numeric.*;
+import gov.nasa.jpf.vm.ChoiceGenerator;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.StackFrame;
+import gov.nasa.jpf.vm.ThreadInfo;
+
+public class ARRAYLENGTH extends gov.nasa.jpf.jvm.bytecode.ARRAYLENGTH {
+
+    public Object peekArrayAttr(ThreadInfo ti) {
+        return ti.getTopFrame().getOperandAttr(0);
+    }
+
+    @Override
+    public Instruction execute (ThreadInfo th) {
+        StackFrame frame = th.getModifiableTopFrame();
+        
+        if (peekArrayAttr(th) == null || !(peekArrayAttr(th) instanceof ArrayExpression)) {
+            return super.execute(th);
+        }
+
+       ArrayExpression arrayAttr = (ArrayExpression)peekArrayAttr(th);
+       frame.pop(1); // We pop the array
+       frame.push(0, false); // The concrete value does not matter
+       frame.setOperandAttr(arrayAttr.length);
+        
+       return getNext(th);
+    }
+}
+       
+        
+
+
