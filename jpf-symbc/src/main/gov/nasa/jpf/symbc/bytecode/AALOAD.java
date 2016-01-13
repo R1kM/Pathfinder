462a4080f1daf2521b6c0a1b7194e7992b915563
R1kM <aymeric.fr@hotmail.fr>
2016-01-14 00:11:24 +0100

    Adding checks for bounds of AALOAD instruction (actual instruction not implemented)

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/bytecode/AALOAD.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/bytecode/AALOAD.java
@@ -21,9 +21,13 @@
 package gov.nasa.jpf.symbc.bytecode;
 
 import gov.nasa.jpf.symbc.SymbolicInstructionFactory;
+import gov.nasa.jpf.symbc.arrays.ArrayExpression;
+import gov.nasa.jpf.symbc.arrays.ObjectSymbolicArray;
+import gov.nasa.jpf.symbc.arrays.SelectExpression;
 import gov.nasa.jpf.symbc.heap.HeapChoiceGenerator;
 import gov.nasa.jpf.symbc.heap.SymbolicInputHeap;
 import gov.nasa.jpf.symbc.numeric.Comparator;
+import gov.nasa.jpf.symbc.numeric.IntegerConstant;
 import gov.nasa.jpf.symbc.numeric.IntegerExpression;
 import gov.nasa.jpf.symbc.numeric.PCChoiceGenerator;
 import gov.nasa.jpf.symbc.numeric.PathCondition;
@@ -47,16 +51,105 @@ public class AALOAD extends gov.nasa.jpf.jvm.bytecode.AALOAD {
   @Override
   public Instruction execute (ThreadInfo ti) {
 	
-	  if (peekIndexAttr(ti)==null || !(peekIndexAttr(ti) instanceof IntegerExpression))
-		  return super.execute(ti);
-	  // index is symbolic
+      if (peekArrayAttr(ti) == null || !(peekArrayAttr(ti) instanceof ArrayExpression)) {
+          // In this case, the array isn't symbolic
+    	  if (peekIndexAttr(ti)==null || !(peekIndexAttr(ti) instanceof IntegerExpression)) {
+              // In this case, the index isn't symbolic either
+	    	  return super.execute(ti);
+          }
+      }
 	  
-	  StackFrame frame = ti.getModifiableTopFrame();
-	  arrayRef = frame.peek(1); // ..,arrayRef,idx
+      ObjectSymbolicArray arrayAttr = null;
+      ChoiceGenerator<?> cg;
+      boolean condition;
+
+      if (!ti.isFirstStepInsn()) { // first time around
+          cg = new PCChoiceGenerator(3);
+          ((PCChoiceGenerator)cg).setOffset(this.position);
+          ((PCChoiceGenerator)cg).setMethodName(this.getMethodInfo().getFullName());
+          ti.getVM().setNextChoiceGenerator(cg);
+          return this;
+      } else { // this is what really returns results
+        cg = ti.getVM().getChoiceGenerator();
+        assert (cg instanceof PCChoiceGenerator) : "expected PCChoiceGenerator, got: " + cg;
+      }
+
+      PathCondition pc;
+      ChoiceGenerator<?> prev_cg = cg.getPreviousChoiceGeneratorOfType(PCChoiceGenerator.class);
+
+      if (prev_cg == null) {
+          pc = new PathCondition();
+      } else {
+          pc = ((PCChoiceGenerator)prev_cg).getCurrentPC();
+      }
+
+      assert pc != null;
+
+      if (peekArrayAttr(ti) == null || !(peekArrayAttr(ti) instanceof ArrayExpression)) {
+          // In this case, the array isn't symbolic
+          if (peekIndexAttr(ti) == null || !(peekIndexAttr(ti) instanceof IntegerExpression)) {
+              // In this case, the index isn't symbolic either
+              return super.execute(ti);
+          }
+          // We have a concrete array, but a symbolic index. We add all the constraints about the elements of the array and perform the select
+          // TODO
+          throw new RuntimeException("AALOAD : Concrete array, symbolic index not handled");
+       } else {
+           arrayAttr = (ObjectSymbolicArray)peekArrayAttr(ti);
+       }
+
+       IntegerExpression indexAttr = null;
+       SelectExpression se = null;
+
+   	   StackFrame frame = ti.getModifiableTopFrame();
+	   arrayRef = frame.peek(1); // ..,arrayRef,idx
+
+       if (peekIndexAttr(ti) == null || !(peekIndexAttr(ti) instanceof IntegerExpression)) {
+           // In this case, the index isn't symbolic
+           index = frame.peek();
+           se = new SelectExpression(arrayAttr, index);
+           indexAttr = new IntegerConstant(index);
+       } else {
+           indexAttr = (IntegerExpression)peekIndexAttr(ti);
+           se = new SelectExpression(arrayAttr, indexAttr);
+       }
+
+       assert arrayAttr != null;
+       assert indexAttr != null;
+       assert se != null;
+
 	    if (arrayRef == MJIEnv.NULL) {
 	      return ti.createAndThrowException("java.lang.NullPointerException");
 	    }
-	  throw new RuntimeException("Arrays: symbolic index not handled");
 	  
+        if ((Integer)cg.getNextChoice()==1) { // check bounds of the index
+            pc._addDet(Comparator.GE, se.index, se.ae.length);
+            if (pc.simplify()) { // satisfiable
+                ((PCChoiceGenerator) cg).setCurrentPC(pc);
+                return ti.createAndThrowException("java.lang.ArrayIndexOutOfBoundsException", "index greater than array bounds");
+            } else {
+                ti.getVM().getSystemState().setIgnored(true);
+                return getNext(ti);
+            }
+        } else if ((Integer)cg.getNextChoice() == 2) {
+            pc._addDet(Comparator.LT, se.index, new IntegerConstant(0));
+            if (pc.simplify()) { // satisfiable
+                ((PCChoiceGenerator) cg).setCurrentPC(pc);
+                return ti.createAndThrowException("java.lang.ArrayIndexOutOfBoundsException", "index smaller than array bounds");
+            } else {
+                ti.getVM().getSystemState().setIgnored(true);
+                return getNext(ti);
+            }
+        } else {
+            pc._addDet(Comparator.LT, se.index, se.ae.length);
+            pc._addDet(Comparator.GE, se.index, new IntegerConstant(0));
+            if (pc.simplify()) { // satisfiable
+                // TODO
+                throw new RuntimeException("AALOAD : Symbolic array not handled");
+            } else {
+                ti.getVM().getSystemState().setIgnored(true);
+                return getNext(ti);
+            }
+        }
   }
 }
