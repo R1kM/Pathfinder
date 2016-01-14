e66eed54cf5882e8cae27b47b7828ef6ed663085
R1kM <aymeric.fr@hotmail.fr>
2016-01-15 00:34:51 +0100

    Including symbolic index in the ObjArray example

--- jpf-symbc/src/examples/arrays/Arrays.java
+++ jpf-symbc/src/examples/arrays/Arrays.java
@@ -21,14 +21,14 @@ public class Arrays {
         return b;
     }
 
-    public static void obj_array(ObjTest[] arr) {
-        int a = 1/(arr[0].y);
+    public static void obj_array(int i, ObjTest[] arr) {
+        int a = 1/(arr[i].y);
     }
 
     public static void main(String[] args) {
         int[] test = {1,2,3};
         ObjTest[] objTest = {new ObjTest(1,2), new ObjTest(1,0)};
-        obj_array(objTest);
+        obj_array(0, objTest);
         int j = counter_bis(1, test);
         int k = counter(1, test);
         int b = check_length(2, test);
